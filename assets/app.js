63739e4123b2a0c9f8cb70563207c0227fd69461
Steve Archuleta <stevearchuleta@me.com>
2019-02-03 13:57:52 -0800

    it works

--- assets/app.js
+++ assets/app.js
@@ -36,17 +36,26 @@ var firstTrain = moment(firstTrainTime, "HH:mm").subtract(1, "years");
 console.log(firstTrain);
 
 //the following statement yields the difference between current time, which is captured as moment(), and the first train time.
-// this code did not work: var diff = moment().diff((firstTrain.diff(now, 'minutes')));
+
+// but... this code did not work: 
+// var diff = moment().diff((firstTrain.diff(now, 'minutes')));
 var diff = moment().diff(moment(firstTrain), "minutes");
+// var diff = moment().diff(firstTrain, 'minutes');
 console.log(diff);
+//convert unix time to a number
+// var timeDiff = moment.unix(diff).format('mm');
+// console.log(timeDiff);
 
 //calculate the remainder of diff mod departure frequency
-var modulusRemainder = diff % Number(departureFrequency);
+var timeSinceLastTrain = diff % Number(departureFrequency);
+console.log(timeSinceLastTrain);
+//time remaining
+var minutesAway = departureFrequency - timeSinceLastTrain;
 console.log("minutes away: ", minutesAway);
 
-var minutesAway = departureFrequency - modulusRemainder;
-
-var nextArrival = moment().add(minutesAway, "minutes");
+//nextArrival. The .format statement below converts an object to a string so that I'll be able to push it to the database.
+// var nextArrival = moment().add(minutesAway, "minutes");
+var nextArrival = moment().add(minutesAway, 'minutes').format('HH:mm');
 console.log(nextArrival);
 
 
@@ -83,9 +92,59 @@ $('#frequency-input').val('');
     NewTableRow.append($('<td>' + dbObject.destination + '</td>'));
     NewTableRow.append($('<td>' + dbObject.frequency + '</td>'));
     // NewTableRow.append($('<td>' + dbObject.time + '</td>'));
-    NewTableRow.append($('<td>' + dbObject.nextArrrival + '</td>'));
+    NewTableRow.append($('<td>' + dbObject.nextArrival + '</td>'));
     NewTableRow.append($('<td>' + dbObject.minutesAway + '</td>'));
   
     $('tbody').append(NewTableRow);
   
   })
+
+//    x-ok, var departureFrequency = '30'
+//    x-ok, var frequency = Number(frequencyFromDOM);
+//    x-ok, var firstTraintTime = '9:30';
+//    x-ok, var firstTrain = moment(firstTrainTime, 'HH:mm');
+
+//    x-ok, var diff = moment().diff(firstTrain, 'minutes');
+
+//    x-ok, var timeSinceLastTrain = diff % frequency;
+//    x-ok, var timeRemaining = frequency - timeSinceLastTrain;
+//    x-ok, var nextArrival = moment().add(timeRemaining, 'minutes').format('HH:mm');
+
+
+
+// real time arrival updates using moment.js
+
+// autoTrain ***************************************************************************
+
+// var tFrequency = 3;
+//     var firstTime = "03:35"; // Time is 3:30 AM
+
+//     // First Time (pushed back 1 year to make sure it comes before current time)
+//     var firstTimeConverted = moment(firstTime,"hh:mm").subtract(1, "years");
+//     console.log(firstTimeConverted);
+
+//     // Current Time
+//     var currentTime = moment();
+//     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+//     // Difference between the times
+//     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+//     console.log("DIFFERENCE IN TIME: " + diffTime);
+
+//     // Time apart (remainder)
+//     var tRemainder = diffTime % tFrequency;
+//     console.log(tRemainder);
+
+//     // Minute Until Train
+//     var tMinutesTillTrain = tFrequency - tRemainder;
+//     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+//     // Next Train
+//     var nextTrain = moment().add(tMinutesTillTrain, "minutes")
+//     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
+
+
+
+
+// autoTrain ***************************************************************************
+
